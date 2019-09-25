12901de08e96127b7fb6b3835e87722bb82c5880
Marek Zdankiewicz <m.zdankiewicz@gecko-lab.com>
2019-09-25 11:44:18 +0200

    Initial commit.

---
+++ AlphaKit/AlphaKit.h
@@ -0,0 +1,18 @@
+//
+//  AlphaKit.h
+//  AlphaKit
+//
+//  Copyright © 2019 AlphaOrg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AlphaKit.
+FOUNDATION_EXPORT double AlphaKitVersionNumber;
+
+//! Project version string for AlphaKit.
+FOUNDATION_EXPORT const unsigned char AlphaKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlphaKit/PublicHeader.h>
+
+
